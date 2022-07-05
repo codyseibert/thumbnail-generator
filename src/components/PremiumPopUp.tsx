00cfd07bcaa471dfef883a18e7e29b85859bc00e
rohanSingh <rohan.singh@codemonk.in>
2022-07-05 19:38:28 +0530

    feat : premium lock

--- src/components/PremiumPopUp.tsx
+++ src/components/PremiumPopUp.tsx
@@ -1,9 +1,12 @@
+import { useTemplateStore } from '@/store/templateStore';
+
 export default function PremiumPopUp(){
 
-
+    const setShowModal = useTemplateStore((state)=>state.setShowModal)
 
     return (
         <>
+        <button onClick={setShowModal(false)}>Close</button>
         <h1 className="h-full w-full bg-red-400">Popup for Premium</h1>
         </>
     )
