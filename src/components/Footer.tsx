1b7fc989a531738384d5d1bfca6a7ef8a9c010ea
Cody Seibert <cseibert113@gmail.com>
2022-07-13 00:07:07 -0400

    removing dark mode issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer id="footer" className="relative z-50 bg-white">
+    <footer id="footer" className="relative z-50 bg-white text-gray-800">
       <div
         tabIndex={0}
         aria-label="footer"
-        className="focus:outline-none border-t border-b border-gray-200 dark:border-gray-700 py-16"
+        className="focus:outline-none border-t border-b border-gray-200 py-16"
       >
         <div className="mx-auto container px-4 xl:px-12 2xl:px-4">
           <div className="lg:flex">
@@ -14,17 +14,17 @@ export default function Footer() {
               <div className="w-full lg:w-1/2 px-6">
                 <ul>
                   <li className="mt-6">
-                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand text-gray-800 ">
                       Templates
                     </a>
                   </li>
                   <li className="mt-6">
-                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand text-gray-800">
                       Pricing
                     </a>
                   </li>
                   <li className="mt-6">
-                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand text-gray-800">
                       FAQ
                     </a>
                   </li>
@@ -33,7 +33,7 @@ export default function Footer() {
               <div className="w-full lg:w-1/2 px-6">
                 <ul>
                   <li>
-                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                    <a className="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand text-gray-800">
                       Free templates
                     </a>
                   </li>
@@ -44,12 +44,12 @@ export default function Footer() {
               <div className="w-full lg:w-1/2 px-6">
                 <ul>
                   <li>
-                    <a className="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                    <a className="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand text-gray-800">
                       Privacy policy
                     </a>
                   </li>
                   <li className="mt-6">
-                    <a className="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                    <a className="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand text-gray-800">
                       Terms of service
                     </a>
                   </li>
@@ -58,29 +58,17 @@ export default function Footer() {
               <div className="w-full lg:w-1/2 px-6 flex flex-col justify-between">
                 <div className="flex items-center mb-6">
                   <a aria-label="Github">
-                    <div className="text-gray-800 dark:text-gray-50 cursor-pointer hover:text-brand dark:hover:text-brand">
+                    <div className="text-gray-800 cursor-pointer hover:text-brand">
                       <img
-                        className="dark:hidden"
                         src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg1.svg"
-                        alt="Github"
-                      />
-                      <img
-                        className="dark:block hidden"
-                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg1dark.svg"
                         alt="Github"
                       />
                     </div>
                   </a>
                   <a aria-label="Twitter" className="ml-4">
-                    <div className="">
+                    <div className="text-gray-800 cursor-pointer hover:text-brand">
                       <img
-                        className="dark:hidden"
                         src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg2.svg"
-                        alt="Twitter"
-                      />
-                      <img
-                        className="dark:block hidden"
-                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg2dark.svg"
                         alt="Twitter"
                       />
                     </div>
