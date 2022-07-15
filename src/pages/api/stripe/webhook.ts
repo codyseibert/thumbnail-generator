a6079052bb66592d55c68ee59f076bdc9e645de1
Cody Seibert <cseibert113@gmail.com>
2022-07-15 02:06:40 -0400

    deploy

--- src/pages/api/stripe/webhook.ts
+++ src/pages/api/stripe/webhook.ts
@@ -1,8 +1,8 @@
 import { NextApiResponse, NextApiRequest } from 'next';
 import { stripe } from '@/libs/stripe';
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 import { prisma } from '@/backend/utils/prisma';
 import { buffer } from "micro";
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
 export const config = {
   api: {
@@ -24,6 +24,7 @@ const webhook = async (
 
   console.log(req.method)
   if (req.method === 'POST') {
+    const buf = await buffer(req);
     const signature = req.headers[
       'stripe-signature'
     ] as string;
@@ -32,7 +33,7 @@ const webhook = async (
       console.log('signature', signature)
       console.log('constructEvent')
       const event = stripe.webhooks.constructEvent(
-        req.body,
+        buf,
         signature,
         webhookSecret
       ) as any;
